f1513799be75016648a281b4e521e97da7fad2f4
SergioDeMonasterio <sergey.monast@gmail.com>
2023-03-02 07:16:22 -0500

    78 PT Insertion - only with one cylinder, new actuator control unit planned

--- lib/pt78_insert_control/pt78_insert_control.cpp
+++ lib/pt78_insert_control/pt78_insert_control.cpp
@@ -3,16 +3,15 @@
 #include "../basic_functions/basic_functions.h"
 
 char *pt78InsertStatesStr[] = {"initState",
-                            "waitOnDetection",
-                            "confirmDetectionPT78Insert",
-                            "holdTubeValveOnPT78Insert",
-                            "plugTipFeederValveOnPT78Insert",
-                            "tubeInsertValveOnPT78Insert",
-                            "holdTubeValveOffPT78Insert",
-                            "tubeInsertValveOff",
-                            "confirmRemovalPT78Insert",
-                            "tubeRemovedPT78Insert"};
-
+                               "waitOnDetection",
+                               "confirmDetectionPT78Insert",
+                               "holdTubeValveOnPT78Insert",
+                               "plugTipFeederValveOnPT78Insert",
+                               "tubeInsertValveOnPT78Insert",
+                               "holdTubeValveOffPT78Insert",
+                               "tubeInsertValveOff",
+                               "confirmRemovalPT78Insert",
+                               "tubeRemovedPT78Insert"};
 
 PT78_InsertCtrlFSM::PT78_InsertCtrlFSM(
     uint8_t inPinCrimperSensor,
@@ -20,7 +19,7 @@ PT78_InsertCtrlFSM::PT78_InsertCtrlFSM(
     uint8_t outPinHoldValve,
     uint8_t outPinInsertValve,
     uint8_t outPinPTFeederValve,
-    
+
     boolean holdTubeValveSwitch,
 
     unsigned int confirmTimeInterval,
@@ -30,7 +29,7 @@ PT78_InsertCtrlFSM::PT78_InsertCtrlFSM(
     unsigned int insertValveOffDelay)
 {
   _inPinCrimperSensor = inPinCrimperSensor;
-  _sensorDetectsOn    = sensorDetectsOn;
+  _sensorDetectsOn = sensorDetectsOn;
   _outPinTubeHoldValve = outPinHoldValve;
   _outPinInsertValve = outPinInsertValve;
   _outPinPTFeederValve = outPinPTFeederValve;
@@ -46,12 +45,14 @@ PT78_InsertCtrlFSM::PT78_InsertCtrlFSM(
   _currentState = initStatePT78Insert;
 }
 
-boolean PT78_InsertCtrlFSM::tubeIsDetected(){
-  return objDetected(_inPinCrimperSensor,_sensorDetectsOn);
+boolean PT78_InsertCtrlFSM::tubeIsDetected()
+{
+  return objDetected(_inPinCrimperSensor, _sensorDetectsOn);
 }
 
-boolean PT78_InsertCtrlFSM::tubeIsRemoved(){
-  return not objDetected(_inPinCrimperSensor,_sensorDetectsOn);
+boolean PT78_InsertCtrlFSM::tubeIsRemoved()
+{
+  return not objDetected(_inPinCrimperSensor, _sensorDetectsOn);
 }
 
 unsigned long PT78_InsertCtrlFSM::getTimeInterval()
@@ -60,7 +61,8 @@ unsigned long PT78_InsertCtrlFSM::getTimeInterval()
   return timeInterval;
 }
 
-boolean PT78_InsertCtrlFSM::timeElapsed(unsigned int time){
+boolean PT78_InsertCtrlFSM::timeElapsed(unsigned int time)
+{
   return getTimeInterval() > time;
 }
 
@@ -100,49 +102,56 @@ void PT78_InsertCtrlFSM::run()
   case confirmDetectionPT78Insert:
     if (tubeIsDetected())
     {
-      if (timeElapsed(_confirmTimeInterval)) changeState(plugTipFeederValveOnPT78Insert);
+      if (timeElapsed(_confirmTimeInterval))
+        changeState(plugTipFeederValveOnPT78Insert);
     }
     else
       changeState(waitOnDetectionPT78Insert);
     break;
   case holdTubeValveOnPT78Insert:
     digitalWrite(_outPinTubeHoldValve, HIGH);
-    if (timeElapsed(_holdTubeValveOnDelay)) changeState(plugTipFeederValveOnPT78Insert);
+    if (timeElapsed(_holdTubeValveOnDelay))
+      changeState(plugTipFeederValveOnPT78Insert);
     break;
   case plugTipFeederValveOnPT78Insert:
     digitalWrite(_outPinPTFeederValve, HIGH);
-    if (timeElapsed(_insertValveOnDelay)) changeState(tubeInsertValveOnPT78Insert);
+    if (timeElapsed(_insertValveOnDelay))
+      changeState(tubeInsertValveOffPT78Insert);
     break;
   case tubeInsertValveOnPT78Insert:
-       digitalWrite(_outPinInsertValve, HIGH);
-       if (timeElapsed(_holdTubeValveOnDelay)) changeState(tubeInsertValveOffPT78Insert);
+    digitalWrite(_outPinInsertValve, HIGH);
+    if (timeElapsed(_holdTubeValveOnDelay))
+      changeState(tubeInsertValveOffPT78Insert);
     break;
   case holdTubeValveOffPT78Insert:
-       if (timeElapsed(_holdTubeValveOffDelay))
-       {
-           digitalWrite(_outPinTubeHoldValve, LOW);
-           changeState(tubeInsertValveOffPT78Insert);
-       }
-       break;
+    if (timeElapsed(_holdTubeValveOffDelay))
+    {
+      digitalWrite(_outPinTubeHoldValve, LOW);
+      changeState(tubeInsertValveOffPT78Insert);
+    }
+    break;
   case tubeInsertValveOffPT78Insert:
     digitalWrite(_outPinInsertValve, LOW);
     digitalWrite(_outPinPTFeederValve, LOW);
-    if (tubeIsRemoved()){
+    if (tubeIsRemoved())
+    {
       changeState(confirmRemovalPT78Insert);
     }
     break;
   case confirmRemovalPT78Insert:
-    if (tubeIsRemoved()) {
-       if (timeElapsed(_confirmTimeInterval)){
+    if (tubeIsRemoved())
+    {
+      if (timeElapsed(_confirmTimeInterval))
+      {
         changeState(tubeRemovedPT78Insert);
-      } 
-    }  
+      }
+    }
     else
       changeState(confirmRemovalPT78Insert);
-    break;  
+    break;
   case tubeRemovedPT78Insert:
-      changeState(waitOnDetectionPT78Insert);
-      break;
+    changeState(waitOnDetectionPT78Insert);
+    break;
   default:
     changeState(waitOnDetectionPT78Insert);
   }
